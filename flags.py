02e96f8f0047e334bb0d17f6d22154108eb640a3
Akash Jaiswal <33419526+jaiakash@users.noreply.github.com>
2021-08-01 21:09:16 +0530

    Added flags

---
+++ flags.py
@@ -0,0 +1,19 @@
+def SoE(n):
+    prime = [True for i in range(n+1)]
+    p = 2
+    while (p * p <= n):
+        if (prime[p] == True):
+            for i in range(p * p, n+1, p):
+                prime[i] = False
+        p += 1
+ 
+    count = 0
+    for p in range(2, n+1):
+        if prime[p]:
+            count+=1
+
+    return count
+
+s=int(input())
+a=SoE(s)
+print(int((a*(a+1))/2))
